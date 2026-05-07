b0c16a078d36e405889e2416d66b44723a9f23b2
AlanL97 <alanl97@nycstudents.net>
2026-05-07 23:06:27 +0000

    final draft

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const byCodeBtn = document.getElementById('code-search');
 const byCapitalBtn = document.getElementById('capital-search');
 const byDemonymBtn = document.getElementById('demonym-search');
 
+const favoriteButton = document.createElement("button");
+favoriteButton.setAttribute("id", "favorite-button");
+favoriteButton.setAttribute("class", "favorite-button");
+favoriteButton.innerHTML = "Add to Favorites";
+if (localStorage.getItem("favorites") === null) {
+    localStorage.setItem("favorites", "");
+}
+const displayFavoritesButton = document.getElementById("display-favorites");
+const resetFavoritesButton = document.getElementById("reset-favorites");
+
+
+let countryInfo = "";
+
 const getCountryByName = async () => {
     const name = document.getElementById('search-bar').value;
     const byNameEndpoint = '/name/';
@@ -12,7 +25,6 @@ const getCountryByName = async () => {
         const response = await fetch(urlToFetch);
         if (response.ok) {
             const countryInfo = await response.json();
-            console.log(countryInfo);
             return countryInfo;
         }
     } catch (error) {
@@ -28,7 +40,6 @@ const getCountryByCode = async () => {
         const response = await fetch(urlToFetch);
         if (response.ok) {
             const countryInfo = await response.json();
-            console.log(countryInfo);
             return countryInfo;
         }
     } catch (error) {
@@ -44,7 +55,6 @@ const getCountryByCapital = async () => {
         const response = await fetch(urlToFetch);
         if (response.ok) {
             const countryInfo = await response.json();
-            console.log(countryInfo);
             return countryInfo;
         }
     } catch (error) {
@@ -60,7 +70,6 @@ const getCountryByDemonym = async () => {
         const response = await fetch(urlToFetch);
         if (response.ok) {
             const countryInfo = await response.json();
-            console.log(countryInfo);
             return countryInfo;
         }
     } catch (error) {
@@ -69,20 +78,20 @@ const getCountryByDemonym = async () => {
 }
 
 byNameBtn.onclick = async function() {
-    const countryInfo = await getCountryByName();
+    countryInfo = await getCountryByName();
     displayCountryInfo(countryInfo);
 }
 byCodeBtn.onclick = async function() {
-    const countryInfo = await getCountryByCode();
+    countryInfo = await getCountryByCode();
     displayCountryInfo(countryInfo);
 
 }
 byCapitalBtn.onclick = async function() {
-    const countryInfo = await getCountryByCapital();
+    countryInfo = await getCountryByCapital();
     displayCountryInfo(countryInfo);
 }
 byDemonymBtn.onclick = async function() {
-    const countryInfo = await getCountryByDemonym();
+    countryInfo = await getCountryByDemonym();
     displayCountryInfo(countryInfo);
 }
 
@@ -109,11 +118,11 @@ const displayCountryInfo = async (countryInfo) => {
 
     const commonCountryName = country.name.common;
     const officialCountryName = country.name.official;
-    const capital = country.capital.join(", ");
+    const capital = country.capital ? country.capital.join(", ") : "None";
     const population = country.population;
-    const currency = Object.values(country.currencies)[0].name;
+    const currency = country.currencies ? Object.values(country.currencies)[0].name : "None";
     const continent = country.continents.join(", ");
-    const border = country.borders.join(", ");
+    const border = country.borders ? country.borders.join(", ") : "None";
     const language = Object.values(country.languages).join(", ");
     const demonym = country.demonyms.eng.m;
 
@@ -129,4 +138,51 @@ const displayCountryInfo = async (countryInfo) => {
         Demonym: ${demonym}
     `;
     factsDiv.appendChild(countryInfoP);
+    factsDiv.appendChild(favoriteButton);
 }
+
+const addToFavorites = (countryInfo) => {
+    let favoritesList;
+    try {
+        favoritesList = JSON.parse(localStorage.getItem("favorites"));
+    } catch {
+        favoritesList = [];
+    }
+    favoritesList.push(countryInfo[0].name.common);
+    localStorage.setItem("favorites", JSON.stringify(favoritesList));
+}
+
+favoriteButton.onclick = function() {
+    addToFavorites(countryInfo);
+}
+
+const displayFavorites = () => {
+    const factsDiv = document.getElementById("country-facts");
+    const flagDiv = document.getElementById("flag");
+    const favorites = document.createElement("p");
+    const header = document.createElement("h3");
+    header.innerHTML = "Favorite Countries";
+    let favoritesList;
+
+    factsDiv.innerHTML = "";
+    flagDiv.innerHTML = "";
+
+    try {
+        favoritesList = JSON.parse(localStorage.getItem("favorites")).join("<br>");
+    } catch (error) {
+        favorites.innerHTML = "No favorites";
+        factsDiv.appendChild(favorites);
+        return;
+    }
+
+    favorites.innerHTML = `${favoritesList}`;
+    factsDiv.appendChild(header);
+    factsDiv.appendChild(favorites);
+}
+
+const resetFavorites = () => {
+    localStorage.setItem("favorites", "");
+}
+
+displayFavoritesButton.onclick = displayFavorites;
+resetFavoritesButton.onclick = resetFavorites;
